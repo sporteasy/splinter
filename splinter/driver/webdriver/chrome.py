9b763f5967c86203a97a3d3f39b39a4df71ed311
Julien Lirochon <julien@lirochon.net>
2012-07-16 19:47:37 +0200

    Accept-Language handling for chrome

--- splinter/driver/webdriver/chrome.py
+++ splinter/driver/webdriver/chrome.py
@@ -5,21 +5,39 @@
 # license that can be found in the LICENSE file.
 
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome import options as chrome_options
 from splinter.driver.webdriver import BaseWebDriver, WebDriverElement
 from splinter.driver.webdriver.cookie_manager import CookieManager
+
+
+class Options(chrome_options.Options):
+
+    def __init__(self):
+        self._prefs = {}
+
+    def add_pref(self, key, value):
+        self._prefs[key] = value
+
+    def to_capabilities(self):
+        capabilities = super(Options, self).to_capabilities()
+        capabilities['chromeOptions']['prefs'] = self._prefs
+        return capabilities
 
 
 class WebDriver(BaseWebDriver):
 
     def __init__(self, user_agent=None, *args, **kwargs):
         self._patch_subprocess()
+
         options = Options()
+        options.add_argument("--disable-translate")
 
         if user_agent is not None:
             options.add_argument("--user-agent=" + user_agent)
 
-        options.add_argument("--disable-translate")
+        if 'accepted_languages' in kwargs:
+            options.add_pref('intl.accept_languages', kwargs['accepted_languages'])
+            del kwargs['accepted_languages']
 
         self.driver = Chrome(chrome_options=options)
         self._unpatch_subprocess()
